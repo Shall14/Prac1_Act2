41e614bb2ccdbe26a7a0f0fcbba59f5db7832fa4
selija <gansy027@mymail.unisa.edu.au>
2022-03-13 00:03:51 +1030

    Add project files.

---
+++ Prac1_Act2/Prac1_Act2.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/* function to read number from standard input (keyboard) */
+void getNumber(int*);
+/* function to calculate the sum of two numbers */
+void calculateSum(int*, int*, int*);
+/* function to display details to the screen */
+void displayDetails(void);
+int main()
+{
+    int num1 = 0; /* first number to be entered by user */
+    int num2 = 0; /* second number to be entered by user */
+    int sum = 0;  /* stores sum of two numbers */
+    displayDetails();
+    /* prompt for and read first number */
+    printf("Please enter first number : ");
+    getNumber(&num1);
+    /* prompt for and read second number */
+    printf("Please enter second number: ");
+    getNumber(&num2);
+    /* add two numbers together and display to screen */
+    calculateSum(&num1, &num2, &sum);
+    printf("\nSum of %d and %d is: %d\n\n", num1, num2, sum);
+    return 0;
+}
+/* function to read number from standard input (keyboard) */
+void getNumber(int* p)
+{
+    scanf("%d", p);
+}
+/* function to calculate the sum of two numbers */
+void calculateSum(int* number1ptr, int* number2ptr, int* sumptr)
+{
+    *sumptr = *number1ptr + *number2ptr;
+}
+/* function to display details to the screen */
+void displayDetails(void)
+{
+    printf("Author : Selija Gangani\n");
+    printf("Stud ID : 110322708\n");
+    printf("Email ID : gansy027@mymail.unisa.edu.au\n");
+    printf("This is my own work as defined by the\n");
+    printf("University's Academic Misconduct Policy.\n\n");
+} /* end function displayDetails */
